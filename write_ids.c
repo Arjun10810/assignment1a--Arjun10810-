e7fa61efb024d9452acc2643861eebe5cbab4406
Arjun Modh <rjune012005@gmail.com>
2024-09-13 12:27:59 -0400

    Added Git_ID and Student_ID

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Arjun10810";
+    char STUDENT_ID[] = "992157379";
 
     return 0 ;
 }
